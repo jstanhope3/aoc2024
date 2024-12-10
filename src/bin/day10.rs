59534df377149853960c810250f661fdcfe8b929
jstanhope3 <jacksonstanhope5921@gmail.com>
2024-12-10 22:47:22 +0100

    day10 with bugs and no solution

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -30,15 +30,24 @@ fn build_flow_map(grid: &Grid) -> FlowMap {
         for j in 0..grid.cells[i].len() {
             let mut elem: Vec<Dir> = Vec::new();
             let current_height: i32 = grid.cells[i][j].to_digit(10).unwrap() as i32;
+            let mut is_end = true;
             if fetch_height(grid, (i as i32 + 1, j as i32)) == current_height + 1 {
                 elem.push(Dir::DOWN);
-            } else if fetch_height(grid, (i as i32 - 1, j as i32)) == current_height + 1 {
+                is_end = false;
+            }
+            if fetch_height(grid, (i as i32 - 1, j as i32)) == current_height + 1 {
                 elem.push(Dir::UP);
-            } else if fetch_height(grid, (i as i32, j as i32 + 1)) == current_height + 1 {
+                is_end = false;
+            }
+            if fetch_height(grid, (i as i32, j as i32 + 1)) == current_height + 1 {
                 elem.push(Dir::RIGHT);
-            } else if fetch_height(grid, (i as i32, j as i32 - 1)) == current_height + 1 {
+                is_end = false;
+            }
+            if fetch_height(grid, (i as i32, j as i32 - 1)) == current_height + 1 {
                 elem.push(Dir::LEFT);
-            } else {
+                is_end = false;
+            }
+            if is_end {
                 elem.push(Dir::STOP);
             }
             row.push(elem);
@@ -50,7 +59,7 @@ fn build_flow_map(grid: &Grid) -> FlowMap {
     }
 }
 
-fn move_loc_by_dir(loc: (i32, i32), dir: Dir) -> (i32, i32) {
+fn move_loc_by_dir(loc: &(i32, i32), dir: &Dir) -> (i32, i32) {
     match dir {
         Dir::DOWN => {
             return (loc.0 + 1, loc.1);
@@ -64,33 +73,82 @@ fn move_loc_by_dir(loc: (i32, i32), dir: Dir) -> (i32, i32) {
         Dir::LEFT => {
             return (loc.0, loc.1 - 1);
         },
-        _ => {return loc;}
+        _ => {return loc.clone();}
     }
 }
 
 fn trace_paths(flow_map: &FlowMap, loc: (i32, i32)) -> Vec<Vec<(i32, i32)>>{
     
-    let all_paths_terminated: bool = false;
+    let mut all_paths_terminated: bool = false;
     let mut paths: Vec<Vec<(i32, i32)>> = Vec::new();
+    let mut terminated_paths: Vec<Vec<(i32, i32)>> = Vec::new();
     let mut path = Vec::new();
     path.push(loc);
     paths.push(path);
+    let mut iter = 0;
     while !all_paths_terminated {
+        all_paths_terminated = true;
         for i in 0..paths.len() {
-            let dirs: Vec<Dir> = flow_map.map[loc.0 as usize][loc.1 as usize].clone();
+            let current_loc = paths[i].last().unwrap().clone();
+            let dirs: Vec<Dir> = flow_map.map[current_loc.0 as usize][current_loc.1 as usize].clone();
+            if dirs.len() == 1 && dirs[0] == Dir::STOP {
+                println!("path terminated");
+                terminated_paths.push(paths[i].clone());
+                paths.remove(i as usize);
+                continue; // path is terminated, continue;
+            }
+            if dirs.len() == 1 {
+                println!("updating path");
+                all_paths_terminated = false;
+                paths[i].push(move_loc_by_dir(&current_loc, &dirs[0]));
+            } else {
+                assert!(dirs.len() > 1); // sanity check
+                println!("branch detected");
+                let currpath = paths[i].clone();
+                paths[i].push(move_loc_by_dir(&current_loc, &dirs[0]));
+                for dir_i in 1..dirs.len() {
+                    let mut new_path = currpath.clone();
+                    new_path.insert(0, move_loc_by_dir(&current_loc, &dirs[dir_i]));
+                    println!("path inserted");
+                    paths.insert(i + 1, new_path);
+                }
+                all_paths_terminated = false;           
+            }
         }
+        iter += 1;
+        println!("iter: {}", iter);
     }
     return paths;
 }
 
-fn trace_flow_map(flow_map: &FlowMap) {
-
+fn score_paths(paths: &Vec<Vec<(i32, i32)>>, grid:&Grid ) -> i32 {
+    let mut sum = 0;
+    for path in paths {
+        let last_pos = path.last().unwrap();
+        if fetch_height(grid, *last_pos) == 9 {
+            sum += 1;
+        }
+    }
+    return sum;
 }
+
+// fn trace_flow_map(flow_map: &FlowMap) {
+    
+// }
 fn main() {
     let path = Path::new("inputs/day10.txt");
     let grid = parse_file_to_grid(path);
 
     let flowmap = build_flow_map(&grid);
-
-    
+    let mut sum = 0;
+    for i in 0..grid.cells.len() {
+        for j in 0..grid.cells[i].len() {
+            if grid.cells[i][j] == '0' {
+                let pos = (i as i32, j as i32);
+                let paths = trace_paths(&flowmap, pos);
+                sum += score_paths(&paths, &grid);
+            }
+        }
+    }
+    println!("score of paths: {}", sum);
 }
