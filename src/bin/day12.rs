fca35b4a07b99a894863e9d5cc0a12670e0ddbd1
jstanhope3 <jacksonstanhope5921@gmail.com>
2024-12-12 22:45:50 +0100

    day12

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -38,6 +38,89 @@ fn count_fences(grid: &Grid, region: &Region) -> i32{
     return num_fences
 }
 
+fn get_bbox(grid: &Grid, region: &Region) -> (Cell, Cell) {
+    let mut min_corner = (grid.num_rows as i32, grid.num_cols as i32);
+    let mut max_corner = (0,0);
+
+    for cell in region.1.iter() {
+        if cell.0 > max_corner.0 {
+            max_corner.0 = cell.0
+        }
+        if cell.1 > max_corner.1 {
+            max_corner.1 = cell.1
+        }
+        if cell.0 < min_corner.0 {
+            min_corner.0 = cell.0
+        }
+        if cell.1 < min_corner.1 {
+            min_corner.1 = cell.1
+        }
+    }
+    
+    return (min_corner, max_corner);
+}
+
+fn count_sides(grid: &Grid, region: &Region) -> i32 {
+    let mut corners: Vec<(f32, f32)> = Vec::new();
+
+    let bbox = get_bbox(grid, region);
+
+    for i in bbox.0.0..bbox.1.0+1 {
+        for j in bbox.0.1..bbox.1.1+1 {
+            let cell = (i, j);
+            let plant = fetch_grid_val(grid, cell);            
+            print!(" {}", plant);
+            // check all 4 cases:
+            if plant == region.0 {
+                if !region.1.contains(&cell) {
+                    continue;
+                }
+                if fetch_grid_val(grid, (i + 1, j)) != region.0 && fetch_grid_val(grid, (i, j + 1)) != region.0 {
+                    corners.push((i as f32 + 0.5f32, j as f32 + 0.5f32));
+                    print!(" c");
+                }
+                if fetch_grid_val(grid, (i + 1, j)) != region.0 && fetch_grid_val(grid, (i, j - 1)) != region.0 {
+                    corners.push((i as f32 + 0.5f32, j as f32 - 0.5f32));
+                    print!(" c");
+                }
+                if fetch_grid_val(grid, (i - 1, j)) != region.0 && fetch_grid_val(grid, (i, j + 1)) != region.0 {
+                    corners.push((i as f32 - 0.5f32, j as f32 + 0.5f32));
+                    print!(" c");
+                }
+                if fetch_grid_val(grid, (i - 1, j)) != region.0 && fetch_grid_val(grid, (i, j - 1)) != region.0 {
+                    corners.push((i as f32 - 0.5f32, j as f32 - 0.5f32));
+                    print!(" c");
+                }
+            } else {
+                if fetch_grid_val(grid, (i + 1, j)) == region.0 && fetch_grid_val(grid, (i, j + 1)) == region.0 && region.1.contains(&(i + 1, j + 1))  {
+                    corners.push((i as f32 + 0.5f32, j as f32 + 0.5f32));
+                    print!(" c");
+                }
+                if fetch_grid_val(grid, (i + 1, j)) == region.0 && fetch_grid_val(grid, (i, j - 1)) == region.0 && region.1.contains(&(i + 1, j - 1)) {
+                    corners.push((i as f32 + 0.5f32, j as f32 - 0.5f32));
+                    print!(" c");
+                }
+                if fetch_grid_val(grid, (i - 1, j)) == region.0 && fetch_grid_val(grid, (i, j + 1)) == region.0 && region.1.contains(&(i - 1, j + 1)) {
+                    corners.push((i as f32 - 0.5f32, j as f32 + 0.5f32));
+                    print!(" c");
+                }
+                if fetch_grid_val(grid, (i - 1, j)) == region.0 && fetch_grid_val(grid, (i, j - 1)) == region.0 && region.1.contains(&(i - 1, j - 1))  {
+                    corners.push((i as f32 - 0.5f32, j as f32 - 0.5f32));
+                    print!(" c");
+                }
+            }
+        }
+    }
+    // let mut new_corners: Vec<(f32, f32)> = Vec::new();
+    // for corner in corners {
+    //     if !new_corners.contains(&corner) {
+    //         new_corners.push(corner);
+    //     }
+    // }
+    println!("");
+    return corners.len() as i32;
+}
+
 fn grow_region(grid: &Grid, region: &mut Region) {
     let mut is_finished = false;
     while !is_finished {
@@ -91,11 +174,22 @@ fn main() {
         }
     }
     let mut price = 0;
-    for region in regions {
+    for region in regions.iter() {
         let num_fences = count_fences(&grid, &region);
         let local_price: i32 = region.1.len() as i32 * num_fences;
         price += local_price;
         println!("A region of {} plants with price {} * {} = {}", region.0, region.1.len(), num_fences, local_price);
     }
-    println!("total price is {}", price)
+    println!("total price is {}", price);
+
+    println!("--------------------------------------");
+
+    price = 0;
+    for region in regions.iter() {
+        let num_fences = count_sides(&grid, &region);
+        let local_price: i32 = region.1.len() as i32 * num_fences;
+        price += local_price;
+        println!("A region of {} plants with price {} * {} = {}", region.0, region.1.len(), num_fences, local_price);
+    }
+    println!("total price is {}", price);
 }
